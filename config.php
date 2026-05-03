939e559e6bb1461a20230128695d3454fb5ebad3
DarilFerdiansyah <Arilpandan11111@gmail.com>
2026-05-03 22:07:55 +0700

    "Update validasi saldo dan cetak laporan"

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
-$conn = mysqli_connect("SQLxxx.infinityfree.com", "if0_xxxxxxx", "PasswordLu", "if0_xxxxxxx_db_keuangan");
+$conn = mysqli_connect("localhost", "root", "", "db_keuangan");
+
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
